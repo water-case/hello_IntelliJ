f11ec8e4a06cc515d3d43d95758287a44075c156
TaeHwan <wjd30142@naver.com>
2022-06-27 22:32:27 +0900

    JPA

--- src/test/java/org/example/springboot/web/HelloControllerTest.java
+++ src/test/java/org/example/springboot/web/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package org.example.springboot.web;
 
-import org.example.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
